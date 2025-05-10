137e9f260e9101618e3f58322caef8cccf47c8c4
valeritadb <vbs0005@alu.medac.es>
2025-05-11 00:12:39 +0200

    ventana de cookies intento 1

--- estilos/script.js
+++ estilos/script.js
@@ -6,9 +6,24 @@ console.log("probandoooooo");
 
 window.addEventListener("scroll", function(){
     var header = document.querySelector(".barra-navegacion");
-    if(window.scrollY > 50) {
+    if(window.scrollY > 100) {
         header.classList.add("scrolled");
     } else {
         header.classList.remove("scrolled");
     }
+});
+
+document.addEventListener("DOMContentLoaded", function(){
+    var popupCookies = document.getElementById("popupCookies");
+    var aceptarCookies = document.getElementById("aceptarCookies");
+
+    //Mostar la ventana solo si no se han aceptado las cookies
+    if (!localStorage.getItem(cookiesAceptadas)){
+        popupCookies.style.display = "flex";
+    }
+
+    aceptarCookies.addEventListener("click", function(){
+        localStorage.setItem("cookiesAceptadas", "true");
+        popupCookies.style.display = "none";
+    });
 });
