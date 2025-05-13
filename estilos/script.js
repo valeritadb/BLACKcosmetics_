12d56fcc5c636c01e031a7dd7c9f3491f23b6643
valeritadb <vbs0005@alu.medac.es>
2025-05-13 22:49:29 +0200

    el carrito existe pero no funciona

--- estilos/script.js
+++ estilos/script.js
@@ -33,6 +33,7 @@ document.addEventListener("DOMContentLoaded", function(){
     });
 });
 
+/*
 // Función para mostrar el pop-up al cargar la página
 window.onload = function() {
     document.getElementById('popup').style.display = 'block';
@@ -44,6 +45,48 @@ function cerrarPopup() {
     document.getElementById('popup').style.display = 'none';
     document.getElementById('overlay').style.display = 'none';
 }
+*/
 
+window.onload = function(){
+    if (!localStorage.getItem('popupMostrado')) { //mostrar el popup si no se mostró antes
+        setTimeout(function(){
+            document.getElementById('popup').style.display = 'block';
+            document.getElementById('overlay').style.display = 'block';
+            localStorage.setItem('popupMostrado', true); //guarda el estado en el navegador
+        }, 7000);//3 segundos
+    }
+};
 
+//Funcion para cerrar el popup
+function validarEnvio(){
+    let terminosAceptados = document.getElementById('terminos').checked;
+    let emailIngresado = document.getElementById('campo-email').value.trim();
 
+    if (!terminosAceptados){
+        alert("Debes aceptar los términos y condiciones");
+        return;
+    }
+}
+
+// Función para cerrar el pop-up
+function cerrarPopup() {
+    document.getElementById('popup').style.display = 'none';
+    document.getElementById('overlay').style.display = 'none';
+}
+
+//funciones del carrito
+document.addEventListener("DOMContentLoaded", function () {
+    const iconoCarrito = document.querySelector(".fa-shopping-bag");
+    const carrito = document.getElementById("carrito");
+    const cerrarCarrito = document.getElementById("cerrar-carrito");
+
+    // Mostrar carrito al hacer clic en el icono
+    iconoCarrito.addEventListener("click", function () {
+        carrito.classList.add("activo");
+    });
+
+    // Cerrar carrito al hacer clic en el botón
+    cerrarCarrito.addEventListener("click", function () {
+        carrito.classList.remove("activo");
+    });
+});
